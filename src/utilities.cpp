e8b6fe0679b8778b542b42962e022d737d8265d8
Anduri1 <noah@sieck.org>
2020-09-12 19:09:03 -0700

    Saving

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -13,7 +13,9 @@
 #include "stdlib.h"
 
 using namespace std;
-// testing!
+
+vector<process_stats> vec;
+
 //********************** private to this compilation unit **********************
 
 //if myString does not contain a string rep of number returns o
@@ -23,7 +25,31 @@ int stringToInt(const char *myString) {
 }
 
 int loadData(const char* filename, bool ignoreFirstRow) {
+	ifstream file;
+	process_stats stats;
+	string str;
+	if (file.open(filename)) {}
+	else {return COULD_NOT_OPEN_FILE;}
 
+	if (ignoreFirstRow) {
+		vec.erase(vec.begin());
+	}
+
+	while (!file.eof()) {
+		string sub;
+		getline(file, sub, CHAR_TO_SEARCH_FOR);
+		//cout << sub << endl;
+		//Somehow parse stuff here
+
+	}
+
+	vec.push_back(stats);
+	file.close();
+/*
+	if(vec.empty()) {
+		return FAIL;
+	}
+*/
 	return SUCCESS;
 }
 
@@ -35,13 +61,14 @@ void sortData(SORT_ORDER mySortOrder) {
 
 process_stats getNext() {
 	process_stats myFirst;
-
+	myFirst = vec.front(); //Front is a reference to first vector element
+	vec.erase(vec.begin()); //Begin is a pointer to first vector element
 	return myFirst;
 }
 
 //returns number of process_stats structs in the vector holding them
 int getNumbRows(){
-	return 0;
+	return vec.size();
 }
 
 

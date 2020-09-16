2e85f290e2276e698323db96ba22b4b060f9f838
Anduri1 <noah@sieck.org>
2020-09-15 17:29:03 -0700

    I HAVE NO IDEA WHATS HAPPENING

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -27,46 +27,59 @@ int stringToInt(const char *myString) {
 int loadData(const char* filename, bool ignoreFirstRow) {
 	ifstream file;
 	process_stats stats;
-	int count = 0;
+	bool ignore = true;
 	file.open(filename);
-	if (file.fail()) {return COULD_NOT_OPEN_FILE;}
+	string line;
+	//if (!file) {
+		//return COULD_NOT_OPEN_FILE;
+	//}
 	int ch;
-	while ((ch = file.get()) != EOF) { //!eof() does not work, I think
-		if (ignoreFirstRow && count == 0) {
-				cout << "Skipped" << endl;
-				count += 1;
+	while ((ch = file.get())) { //!eof() does not work, I think. Also try (ch = file.get()) != EOF ALSO .good()
+		//cout << "ENTERED";
+		if (ignoreFirstRow and ignore) {
+				getline(file, line); // Should skip
+				cout << "SKIP :" << line << endl;
+				//cout << "Skipped" << endl;
+				ignore = false;
+
 			} else {
 				vector<int> str;
-				string sub;
-				getline(file, sub, CHAR_TO_SEARCH_FOR);
-				//cout << sub << endl;
-				//Somehow parse stuff here
-				int convert = stoi(sub);
-				str.push_back(convert);
-				for (int i = 0; i < str.size(); i++) {
-					stats.process_number = str[0];
-					stats.start_time = str[1];
-					stats.cpu_time = str[2];
-					stats.io_time = str[3];
+				// WHY is this being skipped??????? No sense??????
+				while(getline(file, line)) {
+
+					cout << "HERE: " << line << endl;
 				}
-				if (stats.cpu_time == UNINITIALIZED || stats.io_time  == UNINITIALIZED || stats.process_number  == UNINITIALIZED || stats.start_time  == UNINITIALIZED) {
+				while (getline(file, line)) {
+					if (line == "/n") {
+						break;
+					}
+					cout << "MADE IT";
+					istringstream sub(line);
+					string s;
+					getline(sub, s, CHAR_TO_SEARCH_FOR);
+					//Somehow parse stuff here
+					int convert = stoi(s);
+					cout << "Convert: " << convert;
+					str.push_back(convert);
+					//cout << str;
+
+				}
+				//if (stats.cpu_time == UNINITIALIZED || stats.io_time  == UNINITIALIZED || stats.process_number  == UNINITIALIZED || stats.start_time  == UNINITIALIZED) {
+				if (str.size() < 4) {
 						cout << "Corrupt line" << endl;
 					} else {
+						stats.process_number = str[0];
+						stats.start_time = str[1];
+						stats.cpu_time = str[2];
+						stats.io_time = str[3];
+
 						vec.push_back(stats);
-						cout << "Line added" << endl;
+						cout << "Line added";
 					}
 			}
 	}
-
-
-
-
+	//cout << "DONE";
 	file.close();
-/*
-	if(vec.empty()) {
-		return FAIL;
-	}
-*/
 	return SUCCESS;
 }
 
@@ -77,16 +90,16 @@ void sortData(SORT_ORDER mySortOrder) {
 		//for (int j = 0; j < 4; j++) {
 			switch(mySortOrder) {
 				case CPU_TIME:
-					sort(vec.begin(), vec.end(), [](const process_stats ps1, const process_stats ps2){ return ps1.cpu_time < ps2.cpu_time;}); //Think this works?
+					sort(vec.begin(), vec.end(), [](const process_stats& ps1, const process_stats& ps2){ return ps1.cpu_time < ps2.cpu_time;}); //Think this works?
 					break;
 				case PROCESS_NUMBER:
-					sort(vec.begin(), vec.end(), [](const process_stats ps1, const process_stats ps2){ return ps1.process_number < ps2.process_number;});
+					sort(vec.begin(), vec.end(), [](const process_stats& ps1, const process_stats& ps2){ return ps1.process_number < ps2.process_number;});
 					break;
 				case START_TIME:
-					sort(vec.begin(), vec.end(), [](const process_stats ps1, const process_stats ps2){ return ps1.start_time < ps2.start_time;});
+					sort(vec.begin(), vec.end(), [](const process_stats& ps1, const process_stats& ps2){ return ps1.start_time < ps2.start_time;});
 					break;
 				case IO_TIME:
-					sort(vec.begin(), vec.end(), [](const process_stats ps1, const process_stats ps2){ return ps1.io_time < ps2.io_time;});
+					sort(vec.begin(), vec.end(), [](const process_stats& ps1, const process_stats& ps2){ return ps1.io_time < ps2.io_time;});
 					break;
 			}
 		//}
@@ -102,5 +115,6 @@ process_stats getNext() {
 
 //returns number of process_stats structs in the vector holding them
 int getNumbRows(){
-	return vec.size();
+	int x = vec.size();
+	return x;
 }

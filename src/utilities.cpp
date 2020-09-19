1f5e34b4f26ce513d0c7e323c22843c7fe777b41
Anduri1 <noah@sieck.org>
2020-09-18 19:19:16 -0700

    DONE!

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -25,85 +25,74 @@ int stringToInt(const char *myString) {
 }
 
 int loadData(const char* filename, bool ignoreFirstRow) {
-	ifstream file;
-	process_stats stats;
-	bool ignore = true;
-	file.open(filename);
-	string line;
-	//if (!file) {
-		//return COULD_NOT_OPEN_FILE;
-	//}
-	int ch;
-	while ((ch = file.get())) { //!eof() does not work, I think. Also try (ch = file.get()) != EOF ALSO .good()
-		//cout << "ENTERED";
-		if (ignoreFirstRow and ignore) {
-				getline(file, line); // Should skip
-				cout << "SKIP :" << line << endl;
-				//cout << "Skipped" << endl;
-				ignore = false;
+	try {
+		bool ignore = true;
+		string line;
+		ifstream file(filename);
 
-			} else {
-				vector<int> str;
-				// WHY is this being skipped??????? No sense??????
-				while(getline(file, line)) {
-
-					cout << "HERE: " << line << endl;
-				}
-				while (getline(file, line)) {
-					if (line == "/n") {
-						break;
-					}
-					cout << "MADE IT";
+		if (!file.is_open()) {
+			return COULD_NOT_OPEN_FILE;
+		}
+		vec.clear();
+		vector<int> str;
+		while (file.peek() != ifstream::traits_type::eof()) {
+			if (ignoreFirstRow and ignore) {
+					getline(file, line); // Should skip
+					ignore = false;
+				} else {
+					str.clear();
+					getline(file, line);
 					istringstream sub(line);
 					string s;
-					getline(sub, s, CHAR_TO_SEARCH_FOR);
-					//Somehow parse stuff here
-					int convert = stoi(s);
-					cout << "Convert: " << convert;
-					str.push_back(convert);
-					//cout << str;
-
-				}
-				//if (stats.cpu_time == UNINITIALIZED || stats.io_time  == UNINITIALIZED || stats.process_number  == UNINITIALIZED || stats.start_time  == UNINITIALIZED) {
-				if (str.size() < 4) {
-						cout << "Corrupt line" << endl;
-					} else {
-						stats.process_number = str[0];
-						stats.start_time = str[1];
-						stats.cpu_time = str[2];
-						stats.io_time = str[3];
-
-						vec.push_back(stats);
-						cout << "Line added";
+					while (sub.good()) {
+						getline(sub, s, CHAR_TO_SEARCH_FOR);
+						if (s == "") {
+							continue;
+						} else {
+							int convert = stringToInt(s.c_str());
+							str.push_back(convert);
+						}
 					}
-			}
-	}
-	//cout << "DONE";
-	file.close();
-	return SUCCESS;
+
+					if (str.size() < 4) {
+						continue;
+						//cout << "Corrupt line" << endl;
+						} else {
+							process_stats stats;
+							stats.process_number = str[0];
+							stats.start_time = str[1];
+							stats.cpu_time = str[2];
+							stats.io_time = str[3];
+
+							vec.push_back(stats);
+							//str.clear();
+							//cout << "Line added" << endl;
+						}
+				}
+		}
+
+		file.close();
+		return SUCCESS;
+	} catch (fstream::failure& err) { return COULD_NOT_OPEN_FILE;}
 }
 
 
 //will sort according to user preference
 void sortData(SORT_ORDER mySortOrder) {
-	//for (int i = 0; i < vec.size(); i++) {
-		//for (int j = 0; j < 4; j++) {
-			switch(mySortOrder) {
-				case CPU_TIME:
-					sort(vec.begin(), vec.end(), [](const process_stats& ps1, const process_stats& ps2){ return ps1.cpu_time < ps2.cpu_time;}); //Think this works?
-					break;
-				case PROCESS_NUMBER:
-					sort(vec.begin(), vec.end(), [](const process_stats& ps1, const process_stats& ps2){ return ps1.process_number < ps2.process_number;});
-					break;
-				case START_TIME:
-					sort(vec.begin(), vec.end(), [](const process_stats& ps1, const process_stats& ps2){ return ps1.start_time < ps2.start_time;});
-					break;
-				case IO_TIME:
-					sort(vec.begin(), vec.end(), [](const process_stats& ps1, const process_stats& ps2){ return ps1.io_time < ps2.io_time;});
-					break;
-			}
-		//}
-	//}
+	switch(mySortOrder) {
+		case CPU_TIME:
+			sort(vec.begin(), vec.end(), [](const process_stats& ps1, const process_stats& ps2){ return ps1.cpu_time < ps2.cpu_time;}); //Think this works?
+			break;
+		case PROCESS_NUMBER:
+			sort(vec.begin(), vec.end(), [](const process_stats& ps1, const process_stats& ps2){ return ps1.process_number < ps2.process_number;});
+			break;
+		case START_TIME:
+			sort(vec.begin(), vec.end(), [](const process_stats& ps1, const process_stats& ps2){ return ps1.start_time < ps2.start_time;});
+			break;
+		case IO_TIME:
+			sort(vec.begin(), vec.end(), [](const process_stats& ps1, const process_stats& ps2){ return ps1.io_time < ps2.io_time;});
+			break;
+	}
 }
 
 process_stats getNext() {
